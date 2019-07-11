57d5bfe6074ddb63f581801fa740934ead20bcff
trox115 <tofernandes67@gmail.com>
2019-07-11 19:10:35 +0100

    my select finished

--- methods.rb
+++ methods.rb
@@ -1,25 +1,35 @@
 module Enumerable
   def my_each
     i = 0
-    until self[i]==nil
+    until self[i].nil?
       yield(self[i])
       i += 1
     end
   end
 
   def my_each_with_index
-    i=0
-    until self[i]==nil
-        yield(self[i],i)
-        i+=1
+    i = 0
+    until self[i].nil?
+      yield(self[i], i)
+      i += 1
     end
-end
+  end
 
+  def my_select
+    ari = []
+    my_each do |x|
+      ari.push(x) if yield x
+    end
+    ari
+  end
 end
 
 myarray = [0, 1, 3, 10, 5]
-#myarray.each{ |x| puts x }
-myarray.my_each { |x| puts x }
+# myarray.each{ |x| puts x }
+# myarray.my_each { |x| puts x }
 
-#myarray.each_with_index{|x, i| puts i.to_s+" "+x.to_s}
-myarray.my_each_with_index{|x,i| puts i.to_s+" "+x.to_s}
+# myarray.each_with_index{|x, i| puts i.to_s+" "+x.to_s}
+# myarray.my_each_with_index{|x,i| puts i.to_s+" "+x.to_s}
+
+#print myarray.select(&:even?)
+#print myarray.my_select {  |num| num > 2  }
