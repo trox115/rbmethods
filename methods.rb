77d4c843ac387fdaeb1a11b894b94ae75b36ddb3
trox115 <tofernandes67@gmail.com>
2019-07-11 18:38:29 +0100

    finished my_each_with_index

--- methods.rb
+++ methods.rb
@@ -1,13 +1,25 @@
 module Enumerable
   def my_each
     i = 0
-    while i < size
+    until self[i]==nil
       yield(self[i])
       i += 1
     end
   end
+
+  def my_each_with_index
+    i=0
+    until self[i]==nil
+        yield(self[i],i)
+        i+=1
+    end
+end
+
 end
 
 myarray = [0, 1, 3, 10, 5]
-
+#myarray.each{ |x| puts x }
 myarray.my_each { |x| puts x }
+
+#myarray.each_with_index{|x, i| puts i.to_s+" "+x.to_s}
+myarray.my_each_with_index{|x,i| puts i.to_s+" "+x.to_s}
